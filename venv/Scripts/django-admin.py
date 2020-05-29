b28d94826bdf6e50418588cdc21b63e275dad590
atp800 <a.panayiotou17@hotmail.com>
2020-05-29 20:01:57 +0100

    initialisation

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\apana\Documents\Uni\Computer Science\Year 2\Bridging Coursework\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
