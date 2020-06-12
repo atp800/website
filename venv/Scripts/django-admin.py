182429b011b79713166cf24f466dab55a04d7c3f
atp800 <a.panayiotou17@hotmail.com>
2020-06-12 15:19:57 +0100

    initialising 3

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\apana\Documents\Uni\Computer Science\Year 2\Bridging Coursework\venv\Scripts\python.exe
+#!c:\users\apana\documents\uni\computer science\year 2\bridging coursework\venv\scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
