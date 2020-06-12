87f537716bfd4c490d1a4bd9b1908765b257fa03
atp800 <a.panayiotou17@hotmail.com>
2020-06-12 17:02:08 +0100

    Settings config, post model created, test pots

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yz=xbkdc$8#-d^6+ln)+lraa+i-hgs_*y1lcxa(jku6h$_l38u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
