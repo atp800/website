58dfd0bc5561f921d7f3e943a47d6d4b1c1dac02
atp800 <a.panayiotou17@hotmail.com>
2020-06-13 00:53:24 +0100

    Debug set to false

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yz=xbkdc$8#-d^6+ln)+lraa+i-hgs_*y1lcxa(jku6h$_l38u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
