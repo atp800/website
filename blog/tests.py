451c031cf7fa57ccea95ecb5cd83039ac5a97494
atp800 <a.panayiotou17@hotmail.com>
2020-07-27 23:13:19 +0100

    Added more tests
    -added tests for edit and new views

--- blog/tests.py
+++ blog/tests.py
@@ -1,13 +1,26 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from blog.views import post_list
 from blog.views import post_detail
+from blog.views import post_edit
+from blog.views import post_new
+
 
 class PostListTest(TestCase):
 
     def test_root_url_resolves_to_post_list_view(self):
         found = resolve('/')  
-        self.assertEqual(found.func, post_list)  
+        self.assertEqual(found.func, post_list) 
+
+    def test_post_list_returns_correct_html(self):
+        request = HttpRequest()  
+        response = post_list(request)  
+        html = response.content.decode('utf8')  
+        #self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>2nd Year Blog</title>', html)  
+        self.assertTrue(html.endswith('</html>')) 
 
 
 
@@ -15,4 +28,18 @@ class PostDetailTest(TestCase):
 
     def test_root_url_resolves_to_post_detail_view(self):
         found = resolve('/post/14/')  
-        self.assertEqual(found.func, post_detail)  
+        self.assertEqual(found.func, post_detail) 
+        
+
+class PostEditTest(TestCase):
+
+    def test_root_url_resolves_to_post_edit_view(self):
+        found = resolve('/post/14/edit/')  
+        self.assertEqual(found.func, post_edit) 
+
+
+class PostNewTest(TestCase):
+
+    def test_root_url_resolves_to_post_new_view(self):
+        found = resolve('/post/new/')  
+        self.assertEqual(found.func, post_new)  
--- functional_tests.py
+++ functional_tests.py
@@ -12,8 +12,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):  
         self.browser.get('http:///127.0.0.1:8000')
 
-        self.assertIn('2nd Year Blog', self.browser.title)  
-        self.fail('Finish the test!')  
+        self.assertIn('2nd Year Blog', self.browser.title) 
 
 
 if __name__ == '__main__':  
