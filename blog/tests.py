b7541649b817b6b0f1b365b49c4661849e955a9a
atp800 <a.panayiotou17@hotmail.com>
2020-07-27 15:37:18 +0100

    Updated unit test
    -replaced dummy unit test with tests for post list and post detail views

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,18 @@
+from django.urls import resolve
 from django.test import TestCase
+from blog.views import post_list
+from blog.views import post_detail
 
-class SmokeTest(TestCase):
+class PostListTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_post_list_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, post_list)  
+
+
+
+class PostDetailTest(TestCase):
+
+    def test_root_url_resolves_to_post_detail_view(self):
+        found = resolve('/post/14/')  
+        self.assertEqual(found.func, post_detail)  
